76e1814d3a543b02eb3ddb8e68bac7b54e157f17
Mohsin <mohsinfarhat96@gmail.com>
2025-06-06 10:12:44 -0500

    Initialize IntegrateHub Spring Boot project

---
+++ backend/src/main/java/com/mohsin/integratehub/IntegrateHubApplication.java
@@ -0,0 +1,12 @@
+package com.mohsin.integratehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrateHubApplication.class, args);
+    }
+}
