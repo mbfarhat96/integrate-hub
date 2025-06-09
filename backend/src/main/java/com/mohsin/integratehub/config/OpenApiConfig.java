1c6c684405cb080a4bc430c7f57fbcd3eb2be9a1
Mohsin <mohsinfarhat96@gmail.com>
2025-06-09 11:03:31 -0500

    Add OpenAPI/Swagger configuration

---
+++ backend/src/main/java/com/mohsin/integratehub/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.mohsin.integratehub.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI integrateHubOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IntegrateHub API")
+                        .description("Unified API for external service integrations (weather, GitHub, currency, stocks, news)")
+                        .version("v1.0.0")
+                );
+    }
+}
